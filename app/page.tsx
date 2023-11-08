a809c962d37ef77f3c617f3b1c29a1389512b8c9
diego27fernando72 <diego27fernando72@gmail.com>
2023-11-08 00:11:18 +0000

    prueba

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,20 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import { useUser } from "@auth0/nextjs-auth0/client";
 
+
 export default function Home() {
-  const { user, error, isLoading } = useUser();
+  const { user, isLoading, error } = useUser();
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
 
   if (user) {
-    console.log(user)
+
     return (
       <>
-        {user !== undefined ? (
-          <>
-            <div>{user.name}</div>
-            <img src={user.picture} alt="" />
-            <a href="/api/auth/logout">Logout</a>
-          </>
-        ) : null}
+        <div>{user.name}</div>
+        <img src={user.picture} alt="" />
+        <a href="/api/auth/logout">Logout</a>
       </>
     );
   }
